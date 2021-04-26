1d002ea4ba331bd0c8c0dd9b328d57f95e37d58e
zcl0405 <849591483@qq.com>
2021-04-26 21:52:24 +0800

    补充作业

--- 面向对象/fist_day_homework.py
+++ 面向对象/fist_day_homework.py
@@ -2,14 +2,14 @@
 打印小猫爱吃鱼,小猫要喝水
 小猫是一个类,爱吃鱼是一个方法,喝水是一个方法
 '''
-class Cat():
-    def drink_water(self):
-        print('小猫喝水')
-    def eat_fish(self):
-        print('小猫吃鱼')
-cat1=Cat()#创建对象
-cat1.drink_water()#cat1对象调用实例方法
-cat1.eat_fish()#cat1对象调用实例方法
+# class Cat():
+#     def drink_water(self):
+#         print('小猫喝水')
+#     def eat_fish(self):
+#         print('小猫吃鱼')
+# cat1=Cat()#创建对象
+# cat1.drink_water()#cat1对象调用实例方法
+# cat1.eat_fish()#cat1对象调用实例方法
 
 '''
 小明和小美是一个类的两个对象,跑步和吃东西是两个方法,体重属性
@@ -17,28 +17,28 @@ cat1.eat_fish()#cat1对象调用实例方法
 每次吃东西体重会增加1公斤
 '''
 
-
-class people():
-    def __init__(self, weight):
-        self.weight = weight
-        self.new_weight=weight
-    def __str__(self):
-        return f'体重是{self.weight}'
-    def run(self,n):
-        self.new_weight=self.new_weight-n
-        print(f'跑步{n}后体重是{self.new_weight}')
-    def eat(self,m):
-        self.new_weight=self.new_weight+m
-        print(f'吃饭{m}次后体重是{self.new_weight}')
-
-xiaomei = people(75.0)
-xiaoming = people(45.0)
-print(xiaoming)
-xiaoming.run(2)
-xiaoming.eat(2)
-print(xiaomei)
-xiaomei.run(2)
-xiaomei.eat(1)
+#
+# class people():
+#     def __init__(self, weight):
+#         self.weight = weight
+#         self.new_weight=weight
+#     def __str__(self):
+#         return f'体重是{self.weight}'
+#     def run(self,n):
+#         self.new_weight=self.new_weight-n
+#         print(f'跑步{n}后体重是{self.new_weight}')
+#     def eat(self,m):
+#         self.new_weight=self.new_weight+m
+#         print(f'吃饭{m}次后体重是{self.new_weight}')
+#
+# xiaomei = people(75.0)
+# xiaoming = people(45.0)
+# print(xiaoming)
+# xiaoming.run(2)
+# xiaoming.eat(2)
+# print(xiaomei)
+# xiaomei.run(2)
+# xiaomei.eat(1)
 
 '''
 需要定义两个类,一个是房子类,一个是家具类, 房子的属性有:户型和总面积,和家具列表
@@ -75,16 +75,53 @@ print(home)
 士兵可以开火开火就是一个方法,枪可以发射子弹,发射子弹就是一个方法,枪可以填装子弹,填装子弹也是一个方法
 '''
 class Soldier():
-    def fire(self):
-        print('开火')
+    def __init__(self,name):
+        self.name=name
+    def __str__(self):
+        return f'士兵{self.name}开火'
+    def fire(self,item):
+        if item.bullet_number>0:
+            item.fire_bullet
+        else:
+            item.add_bullet
 class Gun():
+    def __init__(self,name):
+        self.name=name
+        self.bullet_number=0
+    def __str__(self):
+        return f'制造一把{self.name},还有{self.bullet_number}发子弹'
     def fire_bullet(self):
-        print('发射子弹')
+        self.bullet_number-=1
+        print(f'发射一枚子弹,还剩{self.bullet_number}发')
     def add_bullet(self):
-        print('增加子弹')
+        self.bullet_number+=20
+        print(f'{self.bullet_number}子弹装填完毕')
+s1=Soldier('ryan')
+gun1=Gun('ak47')
+s1.fire(gun1)
+# gun1.add_bullet()
+# gun1.fire_bullet()
+print(gun1)
 
-ryan=Soldier()
-AK47=Gun()
-AK47.add_bullet()
-AK47.fire_bullet()
-ryan.fire()
+
+
+
+
+
+
+
+
+# class Soldier():
+#     def fire(self):
+#         print('开火')
+# class Gun():
+#     def fire_bullet(self):
+#         print('发射子弹')
+#     def add_bullet(self):
+#         print('增加子弹')
+#
+# ryan=Soldier()
+# AK47=Gun()
+# AK47.add_bullet()
+# AK47.fire_bullet()
+# ryan.fire()
