eabf7c19328c60fe7dd29f6403be8b10782f45d4
zcl0405 <849591483@qq.com>
2021-04-28 20:48:23 +0800

    补充作业

--- 接口第二天/继承.py
+++ 接口第二天/继承.py
@@ -23,24 +23,30 @@
 # xiaoming.gongfu#Tudi是子类继承了Master父类的属性和方法,所以xiaoming可以调用Master的属性
 # xiaoming.make_cake()#xiaoming可以调用父类的方法
 #多继承的意思是一个类同时继承了多个父类,当一个类有多个父类时,默认使用第一个父类同名的属性和方法,同名的
-class Master():
-    def __init__(self):
-        self.gongfu='五香煎饼果子配方'
-    def make_cake(self):
-        print(f'运用{self.gongfu}制作煎饼果子')
-class school():
-    def __init__(self):
-        self.gongfu='香辣煎饼果子的配方'
-    def make_cake(self):
-        print(f'运用{self.gongfu}制作煎饼果子')
-    def make_cake2(self):
-        print(222222)
-class Tudi(Master,school):
-      pass
+# class Master():
+#     def __init__(self):
+#         self.gongfu='五香煎饼果子配方'
+#     def make_cake(self):
+#         print(f'运用{self.gongfu}制作煎饼果子')
+# class school():
+#     def __init__(self):
+#         self.gongfu='香辣煎饼果子的配方'
+#     def make_cake(self):
+#         print(f'运用{self.gongfu}制作煎饼果子')
+#     def make_cake2(self):
+#         print(222222)
+# class Tudi(Master,school):
+#       pass
+# xiaoming=Tudi()
+# print(xiaoming.gongfu)
+# xiaoming.make_cake()
+# xiaoming.make_cake2()
 
-xiaoming=Tudi()
-print(xiaoming.gongfu)
-xiaoming.make_cake()
-xiaoming.make_cake2()
+#子类重写父类同名属性和方法,子类和父类具有同名的属性和方法时,默认使用子类的同名的属性和方法
+class Master();
+    def __init__(self):
 
+
+
+#多肽
 
